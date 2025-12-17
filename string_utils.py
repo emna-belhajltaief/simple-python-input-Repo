b2b08505765dbdfaa38215d9b0cc06f872b6d9d9
emna-belhajltaief <124311650+emna-belhajltaief@users.noreply.github.com>
2025-12-17 14:28:42 +0100

    Add string utility functions for text manipulation

---
+++ string_utils.py
@@ -0,0 +1,13 @@
+def to_upper(text):
+    """Convertit un texte en majuscules"""
+    return text.upper()
+
+
+def to_lower(text):
+    """Convertit un texte en minuscules"""
+    return text.lower()
+
+
+def reverse_string(text):
+    """Inverse une chaîne de caractères"""
+    return text[::-1]
